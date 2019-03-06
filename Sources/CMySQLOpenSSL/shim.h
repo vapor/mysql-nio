524958584e916faa1ffc8da5a3e1565fcda86093
Tanner Nelson <me@tanner.xyz>
2019-03-06 14:12:08 -0500

    openssl shim

--- Sources/CMySQLOpenSSL/shim.h
+++ Sources/CMySQLOpenSSL/shim.h
@@ -3,4 +3,13 @@
 
 #include <openssl/evp.h>
 
+#if (OPENSSL_VERSION_NUMBER < 0x10100000L) || defined(LIBRESSL_VERSION_NUMBER)
+EVP_MD_CTX *EVP_MD_CTX_new(void) {
+	return EVP_MD_CTX_create();
+}
+void EVP_MD_CTX_free(EVP_MD_CTX *ctx) {
+	return EVP_MD_CTX_destroy(ctx);
+}
+#endif
+
 #endif
