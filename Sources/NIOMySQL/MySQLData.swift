83212c92e4a9c4a102a688503b8368d077107dac
Tanner Nelson <me@tanner.xyz>
2019-03-07 19:46:25 -0500

    add MySQLData.Format

--- Sources/NIOMySQL/MySQLData.swift
+++ Sources/NIOMySQL/MySQLData.swift
@@ -1,22 +1,33 @@
 public struct MySQLData {
+    public enum Format {
+        case binary
+        case text
+    }
+    
     public let type: MySQLProtocol.DataType
+    public let format: Format
     public let buffer: ByteBuffer?
     
     public var string: String? {
         guard var buffer = self.buffer else {
             return nil
         }
-        print(self.type)
-        switch self.type {
-        case .MYSQL_TYPE_VARCHAR, .MYSQL_TYPE_VAR_STRING:
+        switch format {
+        case .text:
             return buffer.readString(length: buffer.readableBytes)
         default:
-            return nil
+            switch self.type {
+            case .MYSQL_TYPE_VARCHAR, .MYSQL_TYPE_VAR_STRING:
+                return buffer.readString(length: buffer.readableBytes)
+            default:
+                return nil
+            }
         }
     }
     
-    public init(type: MySQLProtocol.DataType, buffer: ByteBuffer?) {
+    public init(type: MySQLProtocol.DataType, format: Format, buffer: ByteBuffer?) {
         self.type = type
+        self.format = format
         self.buffer = buffer
     }
 }
