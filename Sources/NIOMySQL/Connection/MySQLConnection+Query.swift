73d8251507e5acfa6d1e69ac56fcb9c63c5a4284
Tanner Nelson <me@tanner.xyz>
2019-03-12 18:49:59 -0400

    close statement

--- Sources/NIOMySQL/Connection/MySQLConnection+Query.swift
+++ Sources/NIOMySQL/Connection/MySQLConnection+Query.swift
@@ -17,18 +17,20 @@ private final class MySQLQueryCommand: MySQLCommandHandler {
     
     enum State {
         case ready
-        case params(numParams: Int, numColumns: Int)
-        case columns(numColumns: Int)
+        case params
+        case columns
         case executeColumnCount
         case executeColumns(remaining: Int)
         case rows
         case done
     }
+    
     var state: State
     let binds: [MySQLData]
     let onRow: (MySQLRow) -> ()
     private var columns: [MySQLProtocol.ColumnDefinition41]
     private var params: [MySQLProtocol.ColumnDefinition41]
+    private var ok: MySQLProtocol.COM_STMT_PREPARE_OK?
     
     init(sql: String, binds: [MySQLData], onRow: @escaping (MySQLRow) -> ()) {
         self.state = .ready
@@ -50,36 +52,35 @@ private final class MySQLQueryCommand: MySQLCommandHandler {
         switch self.state {
         case .ready:
             let res = try packet.decode(MySQLProtocol.COM_STMT_PREPARE_OK.self, capabilities: capabilities)
-            
+            self.ok = res
             if res.numParams != 0 {
-                self.state = .params(numParams: numericCast(res.numParams), numColumns: numericCast(res.numColumns))
+                self.state = .params
             } else if res.numColumns != 0 {
-                self.state = .columns(numColumns: numericCast(res.numColumns))
+                self.state = .columns
             } else {
                 self.state = .executeColumnCount
             }
-            
             let execute = MySQLProtocol.COM_STMT_EXECUTE(
                 statementID: res.statementID,
                 flags: [],
                 values: self.binds
             )
             return try .reset([.encode(execute, capabilities: capabilities)])
-        case .params(let numParams, let numColumns):
+        case .params:
             let param = try packet.decode(MySQLProtocol.ColumnDefinition41.self, capabilities: capabilities)
             self.params.append(param)
-            if self.params.count == numParams {
-                if numColumns != 0 {
-                    self.state = .columns(numColumns: numColumns)
+            if self.params.count == numericCast(self.ok!.numParams) {
+                if self.ok!.numColumns != 0 {
+                    self.state = .columns
                 } else {
                     self.state = .executeColumnCount
                 }
             }
             return .noResponse
-        case .columns(let numColumns):
+        case .columns:
             let column = try packet.decode(MySQLProtocol.ColumnDefinition41.self, capabilities: capabilities)
             self.columns.append(column)
-            if self.columns.count == numColumns {
+            if self.columns.count == numericCast(self.ok!.numColumns) {
                 self.state = .executeColumnCount
             }
             return .noResponse
@@ -107,7 +108,9 @@ private final class MySQLQueryCommand: MySQLCommandHandler {
             let data = try MySQLProtocol.BinaryResultSetRow.decode(from: &packet, columns: columns)
             let row = MySQLRow(format: .binary, columns: self.columns, values: data.values)
             self.onRow(row)
-            return .noResponse
+            var packet = MySQLPacket()
+            MySQLProtocol.COM_STMT_CLOSE(statementID: self.ok!.statementID).encode(into: &packet)
+            return .reset([packet])
         case .done: fatalError()
         }
     }
---
+++ Sources/NIOMySQL/Protocol/MySQLProtocol+COM_STMT_CLOSE.swift
@@ -0,0 +1,17 @@
+extension MySQLProtocol {
+    /// COM_STMT_CLOSE deallocates a prepared statement
+    ///
+    /// No response is sent back to the client.
+    ///
+    /// https://dev.mysql.com/doc/internals/en/com-stmt-close.html
+    public struct COM_STMT_CLOSE {
+        /// stmt-id
+        public var statementID: UInt32
+        
+        /// Serializes the `ComStmtClose` into a buffer.
+        public func encode(into packet: inout MySQLPacket) {
+            packet.payload.writeInteger(0x19, endianness: .little, as: UInt8.self)
+            packet.payload.writeInteger(self.statementID, endianness: .little)
+        }
+    }
+}
