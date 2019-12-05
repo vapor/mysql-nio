8b8ed80af7bacdd5b6b19b637fbae89690d9483c
tanner0101 <me@tanner.xyz>
2019-12-05 12:36:55 -0500

    fix ci

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
         .library(name: "MySQLNIO", targets: ["MySQLNIO"]),
     ],
     dependencies: [
-    .package(url: "https://github.com/apple/swift-log.git", from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-log.git", from: "1.0.0"),
         .package(url: "https://github.com/apple/swift-nio.git", from: "2.0.0"),
         .package(url: "https://github.com/apple/swift-nio-ssl.git", from: "2.0.0"),
     ],
@@ -17,7 +17,7 @@ let package = Package(
             pkgConfig: "openssl",
             providers: [
                 .apt(["openssl libssl-dev"]),
-                .brew(["openssl"])
+                .brew(["openssl@1.1"])
             ]
         ),
         .target(name: "MySQLNIO", dependencies: ["CMySQLOpenSSL", "Logging", "NIO", "NIOSSL"]),
