779ff284d5a2420dd3817e91fc96462ee96d7dfd
Gwynne Raskind <gwynne@darkrainfall.org>
2020-03-25 03:07:27 -0500

    In both general usage of ALL MySQLConnections (!) and the tests, ensure that errors that occur very early on don't result in deiniting a connection before it has been closed.

--- Tests/MySQLNIOTests/Utilities.swift
+++ Tests/MySQLNIOTests/Utilities.swift
@@ -3,17 +3,19 @@ import NIOSSL
 
 extension MySQLConnection {
     static func test(on eventLoop: EventLoop) -> EventLoopFuture<MySQLConnection> {
+        let addr: SocketAddress
         do {
-            return try self.connect(
-                to: .makeAddressResolvingHost(env("MYSQL_HOSTNAME") ?? "localhost", port: 3306),
-                username: "vapor_username",
-                database: "vapor_database",
-                password: "vapor_password",
-                tlsConfiguration: .forClient(certificateVerification: .none),
-                on: eventLoop
-            )
+            addr = try SocketAddress.makeAddressResolvingHost(env("MYSQL_HOSTNAME") ?? "localhost", port: 3306)
         } catch {
             return eventLoop.makeFailedFuture(error)
         }
+        return self.connect(
+            to: addr,
+            username: "vapor_username",
+            database: "vapor_database",
+            password: "vapor_password",
+            tlsConfiguration: .forClient(certificateVerification: .none),
+            on: eventLoop
+        )
     }
 }
