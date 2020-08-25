f314b99f3bb8e9d85ad6e9db50b063ccacf06da6
Gwynne Raskind <gwynne@darkrainfall.org>
2020-08-24 20:07:38 -0500

    Make tests respect MYSQL_PORT, MYSQL_USERNAME, MYSQL_DATABASE, MYSQL_PASSWORD

--- Tests/MySQLNIOTests/Utilities.swift
+++ Tests/MySQLNIOTests/Utilities.swift
@@ -5,15 +5,18 @@ extension MySQLConnection {
     static func test(on eventLoop: EventLoop) -> EventLoopFuture<MySQLConnection> {
         let addr: SocketAddress
         do {
-            addr = try SocketAddress.makeAddressResolvingHost(env("MYSQL_HOSTNAME") ?? "localhost", port: 3306)
+            addr = try SocketAddress.makeAddressResolvingHost(
+                env("MYSQL_HOSTNAME") ?? "localhost",
+                port: env("MYSQL_PORT").flatMap(Int.init) ?? 3306
+            )
         } catch {
             return eventLoop.makeFailedFuture(error)
         }
         return self.connect(
             to: addr,
-            username: "vapor_username",
-            database: "vapor_database",
-            password: "vapor_password",
+            username: env("MYSQL_USERNAME") ?? "vapor_username",
+            database: env("MYSQL_DATABASE") ?? "vapor_database",
+            password: env("MYSQL_PASSWORD") ?? "vapor_password",
             tlsConfiguration: .forClient(certificateVerification: .none),
             on: eventLoop
         )
