4587f61d9330b0b77a7b974c843de05a08016ebe
Gwynne Raskind <gwynne@vapor.codes>
2022-03-13 01:01:29 -0600

    Add a number of missing capability flag and column flag definitions, and clean up both sets of declarations a bit.

--- Sources/MySQLNIO/Protocol/MySQLProtocol+CapabilityFlags.swift
+++ Sources/MySQLNIO/Protocol/MySQLProtocol+CapabilityFlags.swift
@@ -2,140 +2,166 @@ extension MySQLProtocol {
     /// The capability flags are used by the client and server to indicate which features they support and want to use.
     ///
     /// https://dev.mysql.com/doc/internals/en/capability-flags.html#packet-Protocol::CapabilityFlags
+    /// https://mariadb.com/kb/en/connection/
+    /// https://github.com/MariaDB/server/blob/10.9/include/mysql_com.h
+    /// https://github.com/mysql/mysql-server/blob/8.0/include/mysql_com.h
     public struct CapabilityFlags: OptionSet, CustomStringConvertible {
         /// Use the improved version of Old Password Authentication.
         /// note: Assumed to be set since 4.1.1.
         /// important: MariaDB **unsets** this flag to signal the presence of extended capability flags
-        public static let CLIENT_LONG_PASSWORD = CapabilityFlags(rawValue: 0x00000001)
+        public static let CLIENT_LONG_PASSWORD = CapabilityFlags(rawValue: 1 << 0)
         
         /// Send found rows instead of affected rows in `EOF_Packet`.
-        public static let CLIENT_FOUND_ROWS = CapabilityFlags(rawValue: 0x00000002)
+        public static let CLIENT_FOUND_ROWS = CapabilityFlags(rawValue: 1 << 1)
         
         /// Longer flags in `Protocol::ColumnDefinition320`.
         /// Server: Supports longer flags.
         /// Client: Expects longer flags.
-        public static let CLIENT_LONG_FLAG = CapabilityFlags(rawValue: 0x00000004)
+        public static let CLIENT_LONG_FLAG = CapabilityFlags(rawValue: 1 << 2)
         
         /// Database (schema) name can be specified on connect in Handshake Response Packet.
         /// Server: Supports schema-name in Handshake Response Packet.
         /// Client: Handshake Response Packet contains a schema-name.
-        public static let CLIENT_CONNECT_WITH_DB = CapabilityFlags(rawValue: 0x00000008)
+        public static let CLIENT_CONNECT_WITH_DB = CapabilityFlags(rawValue: 1 << 3)
         
         /// Server: Do not permit `database.table.column`.
-        public static let CLIENT_NO_SCHEMA = CapabilityFlags(rawValue: 0x00000010)
+        public static let CLIENT_NO_SCHEMA = CapabilityFlags(rawValue: 1 << 4)
         
         /// Compression protocol supported.
         /// Server: Supports compression.
         /// Client: Switches to Compression compressed protocol after successful authentication.
-        public static let CLIENT_COMPRESS = CapabilityFlags(rawValue: 0x00000020)
+        public static let CLIENT_COMPRESS = CapabilityFlags(rawValue: 1 << 5)
         
         /// Special handling of ODBC behavior.
         /// note: No special behavior since 3.22.
-        public static let CLIENT_ODBC = CapabilityFlags(rawValue: 0x00000040)
+        public static let CLIENT_ODBC = CapabilityFlags(rawValue: 1 << 6)
         
         /// Can use `LOAD DATA LOCAL`.
         /// Server: Enables the `LOCAL INFILE` request of `LOAD DATA|XML`.
         /// Client: Will handle `LOCAL INFILE` request.
-        public static let CLIENT_LOCAL_FILES = CapabilityFlags(rawValue: 0x00000080)
+        public static let CLIENT_LOCAL_FILES = CapabilityFlags(rawValue: 1 << 7)
         
         /// Server: Parser can ignore spaces before '('.
         /// Client: Let the parser ignore spaces before '('.
-        public static let CLIENT_IGNORE_SPACE = CapabilityFlags(rawValue: 0x00000100)
+        public static let CLIENT_IGNORE_SPACE = CapabilityFlags(rawValue: 1 << 8)
         
         /// Server: Supports the 4.1 protocol.
         /// Client: Uses the 4.1 protocol.
         /// note: this value was `CLIENT_CHANGE_USER` in 3.22, unused in 4.0
-        public static let CLIENT_PROTOCOL_41 = CapabilityFlags(rawValue: 0x00000200)
+        public static let CLIENT_PROTOCOL_41 = CapabilityFlags(rawValue: 1 << 9)
         
         /// `wait_timeout` versus `wait_interactive_timeout`.
         /// Server: Supports interactive and noninteractive clients.
         /// Client: Client is interactive.
         /// See `mysql_real_connect()`
-        public static let CLIENT_INTERACTIVE = CapabilityFlags(rawValue: 0x00000400)
+        public static let CLIENT_INTERACTIVE = CapabilityFlags(rawValue: 1 << 10)
         
         /// Server: Supports SSL.
         /// Client: Switch to SSL after sending the capability-flags.
-        public static let CLIENT_SSL = CapabilityFlags(rawValue: 0x00000800)
+        public static let CLIENT_SSL = CapabilityFlags(rawValue: 1 << 11)
         
         /// Client: Do not issue `SIGPIPE` if network failures occur (`libmysqlclient` only).
         /// See `mysql_real_connect()`
-        public static let CLIENT_IGNORE_SIGPIPE = CapabilityFlags(rawValue: 0x00001000)
+        public static let CLIENT_IGNORE_SIGPIPE = CapabilityFlags(rawValue: 1 << 12)
         
         /// Server: Can send status flags in `EOF_Packet`.
         /// Client: Expects status flags in `EOF_Packet`.
         /// - Note: This flag is optional in 3.23, but always set by the server since 4.0.
-        public static let CLIENT_TRANSACTIONS = CapabilityFlags(rawValue: 0x00002000)
+        public static let CLIENT_TRANSACTIONS = CapabilityFlags(rawValue: 1 << 13)
         
         /// Unused.
         /// note: Was named `CLIENT_PROTOCOL_41` in 4.1.0.
-        public static let CLIENT_RESERVED = CapabilityFlags(rawValue: 0x00004000)
+        public static let CLIENT_RESERVED = CapabilityFlags(rawValue: 1 << 14)
         
         /// Server: Supports `Authentication::Native41`.
         /// Client: Supports `Authentication::Native41`.
-        public static let CLIENT_SECURE_CONNECTION = CapabilityFlags(rawValue: 0x00008000)
+        public static let CLIENT_SECURE_CONNECTION = CapabilityFlags(rawValue: 1 << 15)
         
         /// Server: Can handle multiple statements per `COM_QUERY` and `COM_STMT_PREPARE`.
         /// Client: May send multiple statements per `COM_QUERY` and `COM_STMT_PREPARE`.
         /// note: Was named `CLIENT_MULTI_QUERIES` in 4.1.0, renamed later.
         /// requires: `CLIENT_PROTOCOL_41`
-        public static let CLIENT_MULTI_STATEMENTS = CapabilityFlags(rawValue: 0x00010000)
+        public static let CLIENT_MULTI_STATEMENTS = CapabilityFlags(rawValue: 1 << 16)
         
         /// Server: Can send multiple resultsets for `COM_QUERY`.
         /// Client: Can handle multiple resultsets for `COM_QUERY`.
         /// requires: `CLIENT_PROTOCOL_41`
-        public static let CLIENT_MULTI_RESULTS = CapabilityFlags(rawValue: 0x00020000)
+        public static let CLIENT_MULTI_RESULTS = CapabilityFlags(rawValue: 1 << 17)
         
         /// Server: Can send multiple resultsets for `COM_STMT_EXECUTE`.
         /// Client: Can handle multiple resultsets for `COM_STMT_EXECUTE`.
         /// requires: `CLIENT_PROTOCOL_41`
-        public static let CLIENT_PS_MULTI_RESULTS = CapabilityFlags(rawValue: 0x00040000)
+        public static let CLIENT_PS_MULTI_RESULTS = CapabilityFlags(rawValue: 1 << 18)
         
         /// Server: Sends extra data in Initial Handshake Packet and supports the pluggable authentication protocol.
         /// Client: Supports authentication plugins.
         /// Requires: `CLIENT_PROTOCOL_41`
-        public static let CLIENT_PLUGIN_AUTH = CapabilityFlags(rawValue: 0x00080000)
+        public static let CLIENT_PLUGIN_AUTH = CapabilityFlags(rawValue: 1 << 19)
         
         /// Server: Permits connection attributes in `Protocol::HandshakeResponse41`.
         /// Client: Sends connection attributes in `Protocol::HandshakeResponse41`.
-        public static let CLIENT_CONNECT_ATTRS = CapabilityFlags(rawValue: 0x00100000)
+        public static let CLIENT_CONNECT_ATTRS = CapabilityFlags(rawValue: 1 << 20)
         
         /// Server: Understands length-encoded integer for auth response data in `Protocol::HandshakeResponse41`.
         /// Client: Length of auth response data in `Protocol::HandshakeResponse41` is a length-encoded integer.
         /// note: The flag was introduced in 5.6.6, but had the wrong value.
-        public static let CLIENT_PLUGIN_AUTH_LENENC_CLIENT_DATA = CapabilityFlags(rawValue: 0x00200000)
+        public static let CLIENT_PLUGIN_AUTH_LENENC_CLIENT_DATA = CapabilityFlags(rawValue: 1 << 21)
         
         /// Server: Announces support for expired password extension.
         /// Client: Can handle expired passwords.
         /// https://dev.mysql.com/doc/internals/en/cs-sect-expired-password.html
-        public static let CLIENT_CAN_HANDLE_EXPIRED_PASSWORDS = CapabilityFlags(rawValue: 0x00400000)
+        public static let CLIENT_CAN_HANDLE_EXPIRED_PASSWORDS = CapabilityFlags(rawValue: 1 << 22)
         
         /// Server: Can set `SERVER_SESSION_STATE_CHANGED` in the Status Flags and send session-state change data after a OK packet.
         /// Client: Expects the server to send sesson-state changes after a OK packet.
-        public static let CLIENT_SESSION_TRACK = CapabilityFlags(rawValue: 0x00800000)
+        public static let CLIENT_SESSION_TRACK = CapabilityFlags(rawValue: 1 << 23)
         
         /// Server: Can send OK after a Text Resultset.
         /// Client: Expects an OK (instead of EOF) after the resultset rows of a Text Resultset.
         /// To support `CLIENT_SESSION_TRACK`, additional information must be sent after all successful commands.
         /// Although the OK packet is extensible, the EOF packet is not due to the overlap of its bytes with the content of the Text Resultset Row.
         /// Therefore, the EOF packet in the Text Resultset is replaced with an OK packet. EOF packets are deprecated as of MySQL 5.7.5.
-        public static let CLIENT_DEPRECATE_EOF = CapabilityFlags(rawValue: 0x01000000)
+        public static let CLIENT_DEPRECATE_EOF = CapabilityFlags(rawValue: 1 << 24)
+        
+        // MARK: - Additional MySQL capabilities added in 8.0
+        
+        /// The client can handle optional metadata information in the resultset.
+        static let CLIENT_OPTIONAL_RESULTSET_METADATA = CapabilityFlags(rawValue: 1 << 25)
+        
+        /// Server: Sets this flag when global variable protocol-compression-algorithms has zstd in its list of supported values.
+        /// Client: Sets this flag when it is configured to use zstd compression method.
+        static let CLIENT_ZSTD_COMPRESSION_ALGORITHM = CapabilityFlags(rawValue: 1 << 26)
+        
+        /// Server: Expects an optional part containing the query parameter set(s). Executes the query for each set of
+        ///         parameters or returns an error if more than 1 set of parameters is sent and the server can't execute it.
+        /// Client: Can send the optional part containing the query parameter set(s).
+        static let CLIENT_QUERY_ATTRIBUTES = CapabilityFlags(rawValue: 1 << 27)
+        
+        /// Server: Sends AuthNextFactor packet after every nth factor authentication method succeeds, except the last factor authentication.
+        /// Client: Reads AuthNextFactor packet sent by server and initiates next factor authentication method.
+        static let MULTI_FACTOR_AUTHENTICATION = CapabilityFlags(rawValue: 1 << 28)
+        
+        /// This flag will be reserved to extend the 32bit capabilities structure to 64bits.
+        static let CLIENT_CAPABILITY_EXTENSION = CapabilityFlags(rawValue: 1 << 29)
+        
+        // MARK: - MariaDB-specific capabilities
         
         /// See: [MariaDB Initial Handshake Packet specific flags](https://mariadb.com/kb/en/library/1-connecting-connecting/)
         
         /// Client support progress indicator (since 10.2).
-        public static let MARIADB_CLIENT_PROGRESS = CapabilityFlags(rawValue: 0x0100000000) // 1 << 32
+        public static let MARIADB_CLIENT_PROGRESS = CapabilityFlags(rawValue: 1 << 32)
         
-        /// Permit `COM_MULTI` protocol.
-        public static let MARIADB_CLIENT_COM_MULTI = CapabilityFlags(rawValue: 0x0200000000) // 1 << 33
+        /// This functionality was experimental and ultimately removed starting in MariaDB 10.6.
+        public static let MARIADB_CLIENT_RESERVED_1 = CapabilityFlags(rawValue: 1 << 33)
         
         /// Permit bulk insert.
-        public static let MARIADB_CLIENT_STMT_BULK_OPERATIONS = CapabilityFlags(rawValue: 0x0400000000) // 1 << 34
+        public static let MARIADB_CLIENT_STMT_BULK_OPERATIONS = CapabilityFlags(rawValue: 1 << 34)
         
         /// Add extended metadata information.
-        public static let MARIADB_CLIENT_EXTENDED_TYPE_INFO = CapabilityFlags(rawValue: 0x0800000000) // 1 << 35
+        public static let MARIADB_CLIENT_EXTENDED_METADATA = CapabilityFlags(rawValue: 1 << 35)
         
         /// Permit skipping metadata.
-        public static let MARIADB_CLIENT_CACHE_METADATA = CapabilityFlags(rawValue: 0x1000000000) // 1 << 36
+        public static let MARIADB_CLIENT_CACHE_METADATA = CapabilityFlags(rawValue: 1 << 36)
         
         /// MariaDB's alternate name for ``CLIENT_LONG_PASSWORD``.
         static let CLIENT_MYSQL = Self.CLIENT_LONG_PASSWORD
@@ -158,40 +184,6 @@ extension MySQLProtocol {
             .CLIENT_LONG_PASSWORD,
             .CLIENT_TRANSACTIONS,
             .CLIENT_SESSION_TRACK,
-        ]
-        
-        /// All capabilities.
-        public static let all: [CapabilityFlags] = [
-            .CLIENT_LONG_PASSWORD,
-            .CLIENT_FOUND_ROWS,
-            .CLIENT_LONG_FLAG,
-            .CLIENT_CONNECT_WITH_DB,
-            .CLIENT_NO_SCHEMA,
-            .CLIENT_COMPRESS,
-            .CLIENT_ODBC,
-            .CLIENT_LOCAL_FILES,
-            .CLIENT_IGNORE_SPACE,
-            .CLIENT_PROTOCOL_41,
-            .CLIENT_INTERACTIVE,
-            .CLIENT_SSL,
-            .CLIENT_IGNORE_SIGPIPE,
-            .CLIENT_TRANSACTIONS,
-            .CLIENT_RESERVED,
-            .CLIENT_SECURE_CONNECTION,
-            .CLIENT_MULTI_STATEMENTS,
-            .CLIENT_MULTI_RESULTS,
-            .CLIENT_PS_MULTI_RESULTS,
-            .CLIENT_PLUGIN_AUTH,
-            .CLIENT_CONNECT_ATTRS,
-            .CLIENT_PLUGIN_AUTH_LENENC_CLIENT_DATA,
-            .CLIENT_CAN_HANDLE_EXPIRED_PASSWORDS,
-            .CLIENT_SESSION_TRACK,
-            .CLIENT_DEPRECATE_EOF,
-            .MARIADB_CLIENT_PROGRESS,
-            .MARIADB_CLIENT_COM_MULTI,
-            .MARIADB_CLIENT_STMT_BULK_OPERATIONS,
-            .MARIADB_CLIENT_EXTENDED_TYPE_INFO,
-            .MARIADB_CLIENT_CACHE_METADATA,
         ]
         
         /// The raw capabilities value.
@@ -224,21 +216,23 @@ extension MySQLProtocol {
             case .CLIENT_CAN_HANDLE_EXPIRED_PASSWORDS: return "CLIENT_CAN_HANDLE_EXPIRED_PASSWORDS"
             case .CLIENT_SESSION_TRACK: return "CLIENT_SESSION_TRACK"
             case .CLIENT_DEPRECATE_EOF: return "CLIENT_DEPRECATE_EOF"
+            case .CLIENT_OPTIONAL_RESULTSET_METADATA: return "CLIENT_OPTIONAL_RESULTSET_METADATA"
+            case .CLIENT_ZSTD_COMPRESSION_ALGORITHM: return "CLIENT_ZSTD_COMPRESSION_ALGORITHM"
+            case .CLIENT_QUERY_ATTRIBUTES: return "CLIENT_QUERY_ATTRIBUTES"
+            case .MULTI_FACTOR_AUTHENTICATION: return "MULTI_FACTOR_AUTHENTICATION"
+            case .CLIENT_CAPABILITY_EXTENSION: return "CLIENT_CAPABILITY_EXTENSION"
             case .MARIADB_CLIENT_PROGRESS: return "MARIADB_CLIENT_PROGRESS"
-            case .MARIADB_CLIENT_COM_MULTI: return "MARIADB_CLIENT_COM_MULTI"
+            case .MARIADB_CLIENT_RESERVED_1: return "MARIADB_CLIENT_RESERVED_1"
             case .MARIADB_CLIENT_STMT_BULK_OPERATIONS: return "MARIADB_CLIENT_STMT_BULK_OPERATIONS"
-            case .MARIADB_CLIENT_EXTENDED_TYPE_INFO: return "MARIADB_CLIENT_EXTENDED_TYPE_INFO"
+            case .MARIADB_CLIENT_EXTENDED_METADATA: return "MARIADB_CLIENT_EXTENDED_METADATA"
             case .MARIADB_CLIENT_CACHE_METADATA: return "MARIADB_CLIENT_CACHE_METADATA"
-            default: return "UNKNOWN(\(self.rawValue))"
+            default: return "UNKNOWN(\(String(self.rawValue, radix: 16))"
             }
         }
         
         /// See `CustomStringConvertible`.
         public var description: String {
-            return CapabilityFlags.all
-                .filter { self.contains($0) }
-                .map { $0.name }
-                .joined(separator: ", ")
+            (0 ..< RawValue.bitWidth).map { .init(rawValue: 1 << $0) }.filter(self.contains(_:)).map(\.name).joined(separator: ", ")
         }
         
         /// MySQL specific flags
--- Sources/MySQLNIO/Protocol/MySQLProtocol+ColumnFlags.swift
+++ Sources/MySQLNIO/Protocol/MySQLProtocol+ColumnFlags.swift
@@ -1,48 +1,141 @@
 extension MySQLProtocol {
-    /// These don't seem to be documented anywhere.
-    public struct ColumnFlags: OptionSet, CustomStringConvertible {
-        /// This column is unsigned.
-        public static let COLUMN_UNSIGNED = ColumnFlags(rawValue: 0b000_0000_0010_0000)
+    /// Partially documented by https://mariadb.com/kb/en/result-set-packets/#field-details-flag
+    ///
+    /// Value definitions taken from:
+    /// - https://github.com/mysql/mysql-server/blob/5.7/include/mysql_com.h#L103-L139
+    /// - https://github.com/mysql/mysql-server/blob/8.0/include/mysql_com.h#L153-L197
+    struct ColumnFlags: OptionSet, CustomStringConvertible {
+        // MARK: Flags
         
-        /// This column is the primary key.
-        public static let PRIMARY_KEY = ColumnFlags(rawValue: 0b000_0000_0000_0010)
+        /// The column has a `NOT NULL` constraint.
+        static let NOT_NULL         = Self(rawValue: 1 <<  0)
         
-        /// This column is not null.
-        public static let COLUMN_NOT_NULL = ColumnFlags(rawValue: 0b000_0000_0000_0001)
+        /// The column is the primary key of its table.
+        static let PRI_KEY          = Self(rawValue: 1 <<  1)
         
-        /// The raw status value.
-        public var rawValue: UInt16
+        /// The column participates in a single-field `UNIQUE KEY`.
+        static let UNIQUE_KEY       = Self(rawValue: 1 <<  2)
         
-        public var name: String {
+        /// The column participates in a key spanning mulitple fields.
+        static let MULTIPLE_KEY     = Self(rawValue: 1 <<  3)
+        
+        /// The column is a `TINYBLOB`, `MEDIUMBLOB`, `BLOB`, or `LONGBLOB`.
+        static let BLOB             = Self(rawValue: 1 <<  4)
+        
+        /// The column's value is unsigned if signedness is meaningful for the type.
+        static let UNSIGNED         = Self(rawValue: 1 <<  5)
+        
+        /// The column has a numeric type with the `ZEROFILL` specifier.
+        static let ZEROFILL         = Self(rawValue: 1 <<  6)
+        
+        /// The column is of string or blob type and has `BINARY` collation.
+        static let BINARY           = Self(rawValue: 1 <<  7)
+        
+        /// The column has type `ENUM`.
+        static let ENUM             = Self(rawValue: 1 <<  8)
+        
+        /// The column has the `AUTOINCREMENT` modifier.
+        static let AUTO_INCREMENT   = Self(rawValue: 1 <<  9)
+        
+        /// The column has type `TIMESTAMP`.
+        static let TIMESTAMP        = Self(rawValue: 1 << 10)
+        
+        /// The column has type `SET`.
+        static let SET              = Self(rawValue: 1 << 11)
+        
+        /// The column has no default value.
+        static let NO_DEFAULT_VALUE = Self(rawValue: 1 << 12)
+        
+        /// The column is set to `NOW()` when updated.
+        static let ON_UPDATE_NOW    = Self(rawValue: 1 << 13)
+        
+        /// The column is of a numeric type (never set by server).
+        static let NUM              = Self(rawValue: 1 << 15)
+        
+        // MARK: Implementation
+        
+        /// The raw column flags.
+        var rawValue: UInt16
+        
+        /// Create a new `ColumnFlags` from the raw value.
+        init(rawValue: UInt16) {
+            self.rawValue = rawValue
+        }
+
+        /// The string representation of a single flag (multiple flags are treated as unknown).
+        var name: String {
             switch self {
-            case .COLUMN_UNSIGNED: return "COLUMN_UNSIGNED"
-            case .PRIMARY_KEY: return "PRIMARY_KEY"
-            case .COLUMN_NOT_NULL: return "COLUMN_NOT_NULL"
-            default: return "UNKNOWN(\(self.rawValue))"
+                case []: return ""
+                case .NOT_NULL: return "NOT_NULL_FLAG"
+                case .PRI_KEY: return "PRI_KEY_FLAG"
+                case .UNIQUE_KEY: return "UNIQUE_KEY_FLAG"
+                case .MULTIPLE_KEY: return "MULTIPLE_KEY_FLAG"
+                case .BLOB: return "BLOB_FLAG"
+                case .UNSIGNED: return "UNSIGNED_FLAG"
+                case .ZEROFILL: return "ZEROFILL_FLAG"
+                case .BINARY: return "BINARY_FLAG"
+                case .ENUM: return "ENUM_FLAG"
+                case .AUTO_INCREMENT: return "AUTO_INCREMENT_FLAG"
+                case .TIMESTAMP: return "TIMESTAMP_FLAG"
+                case .SET: return "SET_FLAG"
+                case .NO_DEFAULT_VALUE: return "NO_DEFAULT_VALUE_FLAG"
+                case .ON_UPDATE_NOW: return "ON_UPDATE_NOW_FLAG"
+                case .NUM: return "NUM_FLAG"
+                default: return "UNKNOWN(\(String(self.rawValue, radix: 16))"
             }
         }
         
-        /// All capabilities.
-        public static var all: [ColumnFlags] {
-            return [
-                .COLUMN_UNSIGNED,
-                .PRIMARY_KEY,
-                .COLUMN_NOT_NULL
-            ]
-        }
-        
         /// `CustomStringConvertible` conformance.
-        public var description: String {
-            return ColumnFlags.all
-                .filter { self.contains($0) }
-                .map { $0.name }
-                .joined(separator: ", ")
-        }
-        
-        /// Create a new `MySQLStatusFlags` from the raw value.
-        public init(rawValue: UInt16) {
-            self.rawValue = rawValue
+        var description: String {
+            (0 ..< RawValue.bitWidth).map { .init(rawValue: 1 << $0) }.filter(self.contains(_:)).map(\.name).joined(separator: ", ")
         }
     }
 
+}
+
+extension MySQLProtocol.ColumnFlags {
+    // MARK: `mysqld` internal column flags
+    
+    // Most of the internal flags require more than 16 bits to represent and thus are not actually expressible
+    // by this type. These defintions are provided solely for completeness; they are not actually useful as written,
+    // or at all, really, unless you're trying to incrementally port `mysqld` to Swift (I wish you luck).
+    
+    /// The column participates in some key.
+    static let _INTERNAL_PART_KEY           = Self(rawValue: UInt16(1 << 14))
+    /// The column is a group field.
+    static let _INTERNAL_GROUP              = Self(rawValue: UInt16(1 << 15))
+    /// The column was flagged as unique during yaccing.
+    static let _INTERNAL_UNIQUE             = Self(rawValue: UInt16(1 << 16))
+    /// The column was flagged as binary-comparable during yaccing.
+    static let _INTERNAL_BINCMP             = Self(rawValue: UInt16(1 << 17))
+    /// The column is being used to fetch fields in an item tree.
+    static let _INTERNAL_GET_FIXED_FIELDS   = Self(rawValue: UInt16(1 << 18))
+    /// The column is part of a parition function.
+    static let _INTERNAL_FIELD_IN_PART_FUNC = Self(rawValue: UInt16(1 << 19))
+    /// The column is part of a new index during alter table.
+    static let _INTERNAL_FIELD_IN_ADD_INDEX = Self(rawValue: UInt16(1 << 20))
+    /// The column is being renamed.
+    static let _INTERNAL_FIELD_IS_RENAMED   = Self(rawValue: UInt16(1 << 21))
+    /// The column has storage media 1.
+    static let _INTERNAL_STORAGE_MEDIA_1    = Self(rawValue: UInt16(1 << 22))
+    /// The column has storage media 2.
+    static let _INTERNAL_STORAGE_MEDIA_2    = Self(rawValue: UInt16(1 << 23))
+    /// The column has storage media 3.
+    static let _INTERNAL_STORAGE_MEDIA_3    = Self(rawValue: UInt16(3 << 22))
+    /// The column has column format 1.
+    static let _INTERNAL_COLUMN_FORMAT_1    = Self(rawValue: UInt16(1 << 24))
+    /// The column has column format 2.
+    static let _INTERNAL_COLUMN_FORMAT_2    = Self(rawValue: UInt16(1 << 25))
+    /// The column has column format 3.
+    static let _INTERNAL_COLUMN_FORMAT_3    = Self(rawValue: UInt16(3 << 24))
+    /// The column is being dropped.
+    static let _INTERNAL_FIELD_IS_DROPPED   = Self(rawValue: UInt16(1 << 26))
+    /// The column has been explcitly specified as `NULL`.
+    static let _INTERNAL_EXPLICIT_NULL_FLAG = Self(rawValue: UInt16(1 << 27))
+    /// The column is `SERIAL` (MySQL 5.7 only, unused in 8.0)
+    static let _INTERNAL_SERIAL             = Self(rawValue: UInt16(1 << 28))
+    /// The column is not loaded in a secondary engine (MySQL 8.0+)
+    static let _INTERNAL_NOT_SECONDARY      = Self(rawValue: UInt16(1 << 29))
+    /// The column has been marked invisible by the user.
+    static let _INTERNAL_FIELD_IS_INVISIBLE = Self(rawValue: UInt16(1 << 30))
 }
