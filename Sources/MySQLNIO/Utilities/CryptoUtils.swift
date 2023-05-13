2b6ef9a976a8caf96097f791b1d14750503273b2
Gwynne Raskind <gwynne@vapor.codes>
2023-05-13 03:10:22 -0500

    Use writeAndFlush() in old connection handler. Massively cleanup old crypto utils and the test for them.

--- Sources/MySQLNIO/Utilities/CryptoUtils.swift
+++ Sources/MySQLNIO/Utilities/CryptoUtils.swift
@@ -1,74 +1,30 @@
 import Crypto
+import Foundation
 import NIOCore
 
 func sha256(_ messages: ByteBuffer...) -> ByteBuffer {
-    let digest = SHA256.hash(data: [UInt8](messages.combine().readableBytesView))
-    var buffer = ByteBufferAllocator().buffer(capacity: SHA256.Digest.byteCount)
-    buffer.writeBytes(digest)
-    return buffer
+    .init(bytes: SHA256.hash(data: [UInt8](messages.combine().readableBytesView)))
 }
 
 func sha1(_ messages: ByteBuffer...) -> ByteBuffer {
-    let digest = Insecure.SHA1.hash(data: [UInt8](messages.combine().readableBytesView))
-    var buffer = ByteBufferAllocator().buffer(capacity: Insecure.SHA1.Digest.byteCount)
-    buffer.writeBytes(digest)
-    return buffer
+    .init(bytes: Insecure.SHA1.hash(data: [UInt8](messages.combine().readableBytesView)))
 }
 
 func xor(_ a: ByteBuffer, _ b: ByteBuffer) -> ByteBuffer {
     assert(a.readableBytes == b.readableBytes)
-    var output = ByteBufferAllocator().buffer(capacity: a.readableBytes)
-    for i in 0..<a.readableBytes {
-        output.writeInteger(a.getInteger(at: i, as: UInt8.self)! ^ b.getInteger(at: i, as: UInt8.self)!)
-    }
-    return output
+    return .init(bytes: zip(a.readableBytesView, b.readableBytesView).map(^))
 }
 
 func xor_pattern(_ a: ByteBuffer, _ b: ByteBuffer) -> ByteBuffer {
-    var output = ByteBufferAllocator().buffer(capacity: a.readableBytes)
-    for i in 0..<a.readableBytes {
-        output.writeInteger(a.getInteger(at: i, as: UInt8.self)! ^ b.getInteger(at: i % b.readableBytes, as: UInt8.self)!)
+    var output = a, outView = output.readableBytesView, bView = b.readableBytesView
+    for i in outView.indices {
+        outView[i] ^= bView[i % bView.count]
     }
     return output
 }
 
 extension Array where Element == ByteBuffer {
     func combine() -> ByteBuffer {
-        switch self.count {
-        case 1: return self[0]
-        default:
-            var base = ByteBufferAllocator().buffer(capacity: 0)
-            self.forEach { buffer in
-                var copy = buffer
-                base.writeBuffer(&copy)
-            }
-            return base
-        }
-    }
-}
-
-private extension OpaquePointer {
-    func convert<T>() -> UnsafePointer<T> {
-        .init(self)
-    }
-    
-    func convert<T>() -> UnsafeMutablePointer<T> {
-        .init(self)
-    }
-    
-    func convert() -> OpaquePointer {
-        self
-    }
-}
-
-private extension UnsafePointer {
-    func convert() -> OpaquePointer {
-        .init(self)
-    }
-}
-
-private extension UnsafeMutablePointer {
-    func convert() -> OpaquePointer {
-        .init(self)
+        self.reduce(into: .init()) { $0.writeImmutableBuffer($1) }
     }
 }
